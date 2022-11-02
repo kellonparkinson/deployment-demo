3c56129968a852d7ccb9deb84b24670df0c80130
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-02 12:12:21 -0600

    pushing updates

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const path = require('path')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+const { home } = require('./controllers/pageCtrl')
+
+app.get('/', home)
 
 const { PORT } = process.env
 
